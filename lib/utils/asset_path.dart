1126e35d9ebf54db60efb08755fc0fa1c9ec8974
ImtiazChowdhury <work.imtiazchowdhury@gmail.com>
2025-08-29 15:46:57 +0600

    404 screen added

--- lib/utils/asset_path.dart
+++ lib/utils/asset_path.dart
@@ -6,4 +6,5 @@ class AppAssetPath {
   static String onboardBackgroundImage = '$baseUrl/onboard.png';
   static String headSetImage = '$baseUrl/head_set.png';
   static String crownIcon = '$baseUrl/crown_icon.png';
+  static String fourZeroFourImage = '$baseUrl/four_zero_four_image.png';
 }
